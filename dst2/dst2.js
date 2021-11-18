95c6377681c820189c5eaca60fcd3b661d4cfe80
Kalle Kaitamäki <aa3470@student.jamk.fi>
2021-11-18 15:28:26 +0200

    Added better comments to dst2

--- dst2/dst2.js
+++ dst2/dst2.js
@@ -1,3 +1,4 @@
+//Luodaan kolmen eri kaupan tuotteista mapit
 const xkauppamap = new Map([
   ['nimi', 'xkauppa'],
   ['lumilapio', 10],
@@ -17,9 +18,12 @@ const zkauppamap = new Map([
   ['karvalakki', 15],
 ]);
 
+//Funktio sum laskee parametrinä annetun mapin tuotteiden hintojen summan
 const sum = function (shop) {
   let sum = 0;
 
+  //Käytetään mapin values() metodia joka palauttaa iteraattori objektin
+  //joka sisältää mapin jokaisen elementin arvon
   for (const value of shop.values()) {
     if (typeof value === 'number') {
       sum += value;
@@ -28,21 +32,34 @@ const sum = function (shop) {
   return sum;
 };
 
+//Funktio jolla rakennetaan summa mappi joka sisältää annetujen kauppojen
+//tuotteiden hintojen summat kaupan nimeen yhdistettynä
+//key kertoo mistä annettujen mappien avain/arvo parista saadaan avain
+//uuden mapin elementeille
 const createSumMap = function (key, ...shops) {
   const sumMap = new Map();
 
   shops.forEach((shop) => {
+    //Otetaan kaupan nimi mparametrin key arvon perusteella
     const name = shop.get(key);
+    //lisätään name avaimeksi ja hyödynnetään sum() funktiota tuotteiden
+    //yhteishinnan laskemiseen.
     sumMap.set(name, sum(shop));
   });
 
   return sumMap;
 };
 
+//minValueMap etsii pienimmän arvon annetusta mapista
 const minValueMap = function (mapX) {
+  //jokaista mapin elementtiä verrataan minValue muuttujan arvoon
+  //alustetaan muuttuja js:n maksimi numeroarvolla koska mapin elementissä
+  //oleva arvo on varmasti tätä pienempi
   let minValue = Number.MAX_VALUE;
+  //minValueKey muuttujaan tallennetaan annetun mapin pienimmän arvon avain.
   let minValueKey = '';
 
+  //käydään mappi läpi forEachilla
   mapX.forEach((value, key) => {
     if (typeof value === 'number' && value < minValue) {
       minValue = value;
@@ -50,6 +67,7 @@ const minValueMap = function (mapX) {
     }
   });
 
+  //palautetaan uusi mappi jossa on tallennettuna pienin arvo ja sen avain
   return new Map([[minValueKey, minValue]]);
 };
 
