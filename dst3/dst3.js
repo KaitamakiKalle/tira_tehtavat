805ee20a5576b1ef22a8b54f3e286f106f82d153
Kalle Kaitamäki <aa3470@student.jamk.fi>
2021-11-18 15:49:47 +0200

    Improved commenting of dst3 and dst4

--- dst3/dst3.js
+++ dst3/dst3.js
@@ -1,20 +1,30 @@
+//Luodaan setit koodareista ja heidän osaamista kielistään
 const coderA = new Set(['JS', 'PHP', 'C#', 'Python']);
 const coderB = new Set(['JS', 'Java', 'C++', 'Python']);
 const coderC = new Set(['JS', 'Java', 'Perl', 'Ruby']);
 
+//Koodarit taulukko sisältää koodarien setit
 const coders = [coderA, coderB, coderC];
 
+//union yhdistää coders taulukossa olevat setit uudeksi setiksi jossa
+//on tieto mitä kieliä koodareiden keskuudessa osataan
 const union = function (coders) {
   const codersUnion = new Set();
 
+  //taulukon läpikäynti
   coders.forEach((coder) => {
+    //Jokaisen coder setin läpikäynti
     coder.forEach((language) => {
+      //Lisätään kieli uuteen settiin. Koska käytänmme settiä ei tarvitse
+      //huolehtia jos joku osaa samaa kieltä kuin toinen koska set tietorakenne
+      //hyväksyy vain uniikkeja arvoja eli jo lisättyjä arvoja ei lisätä uudestaan
       codersUnion.add(language);
     });
   });
   return codersUnion;
 };
 
+//haveCodeskill palauttaa tiedon osaako joku koodareista parametrinä annettua kieltä
 const haveCodeSkill = function (coders, language) {
   const codersUnion = union(coders);
 
--- dst4/dst4.js
+++ dst4/dst4.js
@@ -1,30 +1,44 @@
+//Rakennettaan set tietorakenne itse luokan avulla
 class MySet {
+  //konstruktoriin annetaan parametriksi taulukko. Oletusarvona tyhjä taulukko
   constructor(arr = []) {
+    //tarkistetaan että taulukossa on vain uniikkeja alkioita
     this.arr = this.checkUnique(arr);
+    //size property
     this.size = arr.length;
   }
-
+  //checkUnique luo annetusta taulukosta uuden taulukon jossa on vain uniikkeja alkioita
   checkUnique(arr) {
     const uniques = [];
     arr.forEach((ele) => {
+      //indexOf taulukon metodilla katsotaan ettei alkioita ole vielä lisätty uuteen taulukkoon
       if (uniques.indexOf(ele) === -1) {
         uniques.push(ele);
       }
     });
     return uniques;
   }
+  //add metodilla voidaan lisätä settiin uusi alkio.
   add(data) {
+    //Tarkistus ettei lisättävää alkiota ole jo setissä
     if (this.arr.indexOf(data) === -1) {
       this.arr.push(data);
+      //kasvatetaan myös size propertyn määrää yhdellä
       this.size++;
+      //palautetaan true jos alkio lisättiin onnistuneesti
       return true;
     } else {
+      //palautetaan false jos alkioita ei lisätty esim. jos kyseinen alkio löytyy jo setistä
       return false;
     }
   }
 
+  //Metodi alkion poistamiseen setistä
   remove(data) {
+    //tarkistus että poistettava alkio löytyy setistä
     if (this.arr.indexOf(data) !== -1) {
+      //splice toimii niin että ensimmäinen parametri kertoo mistä kohalta leikkaus aloitetaan ja toinen kuinka
+      //monta "indexiä" poistetaan
       this.arr.splice(this.arr.indexOf(data), 1);
       this.size--;
       return true;
@@ -33,6 +47,7 @@ class MySet {
     }
   }
 
+  //has metodi kertoo onko setissä kysyttyä alkiota
   has(data) {
     if (this.arr.indexOf(data) === -1) {
       return false;
@@ -41,6 +56,7 @@ class MySet {
     }
   }
 
+  //union metodi yhdistää kaksi tai useamman setin toisiinsa
   union(setA, ...sets) {
     sets.forEach((x) => {
       x.arr.forEach((y) => {
